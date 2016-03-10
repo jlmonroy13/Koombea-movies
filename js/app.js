0b552f6a5eed497a608680b7d394bf74f917bebc
Jorge Monroy <jlmonroy13@gmail.com>
2016-03-10 14:15:39 -0500

    Favorites OK

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ $(function() {
 	var allMovies = [];
 	$('.main_leftMenu_categories').change(function(){
 		var selectedOption = $('.main_leftMenu_categories option:selected');
+	/*NOW PLAYING SELECTION---------------------------------------------------------------------*/		
 		if( selectedOption.val() == 'now_playing') {		
 			var url_now_playing = "https://api.themoviedb.org/3/movie/now_playing?api_key=c0b2e256491361d28c75bbe8f9e59a85";
 			$('#info-movie').empty();
@@ -20,33 +21,24 @@ $(function() {
 				$('.cover').css('background-size', 'cover');
 				$.each(data.results, function(i, movie){ 
 					data.results[i].favorite = false;
-					//Request for trailer of each Movie
-						var url_trailer = 'http://api.themoviedb.org/3/movie/'+movie.id+'?api_key=c0b2e256491361d28c75bbe8f9e59a85&append_to_response=videos';
-						function getTrailer(dataTrailer) {
-							data.results[i].trailer = dataTrailer.videos.results[0].key;
-						}
-						$.getJSON(url_trailer, getTrailer);
-					//Request for similar movies of each Movie
-						var url_similars = 'https://api.themoviedb.org/3/genre/'+movie.genre_ids[0]+'/movies?api_key=c0b2e256491361d28c75bbe8f9e59a85';
-						function getSimilars(dataSimilars) { 
-							data.results[i].similars_movies = dataSimilars.results;
-						}
-						$.getJSON(url_similars, getSimilars);
+					
 					//Get only the year
 						var arr = movie.release_date.split('-');
 						var date = arr[0];
 					// Variable to the DOM
 						movieHTML += '<div class="main_leftMenu_moviesList_movie">';
 						movieHTML += '<a class="btn_movie_leftMenu" data-index="'+i+'" href="javascript:void(0)">';
-						movieHTML += '<img src="http://image.tmdb.org/t/p/w500'+movie.poster_path+'">';
+						movieHTML += '<img class="img_movie_leftMenu" src="http://image.tmdb.org/t/p/w500'+movie.poster_path+'">';
 						movieHTML += '</a><h4>'+movie.title+'</h4>';
 						movieHTML += '<span>'+date+'</span>';
 						movieHTML += '</div>';
 				}); // End array cicle
 				$('#leftMenu').html(movieHTML);
 				allMovies = data.results;
+				$('.categories_favorites').remove();
 			};//End getMovies Function
 			$.getJSON(url_now_playing, getMovies);
+	/*POPULAR SELECTION---------------------------------------------------------------------*/			
 		}else if(selectedOption.val() == 'popular') {
 			var url_popular = "https://api.themoviedb.org/3/movie/popular?api_key=c0b2e256491361d28c75bbe8f9e59a85";
 			$('#info-movie').empty();
@@ -58,33 +50,24 @@ $(function() {
 				$('.cover').css('background-size', 'cover');
 				$.each(data.results, function(i, movie){ 
 					data.results[i].favorite = false;
-					//Request for trailer of each Movie
-						var url_trailer = 'http://api.themoviedb.org/3/movie/'+movie.id+'?api_key='+apiKey+'&append_to_response=videos';
-						function getTrailer(dataTrailer) {
-							data.results[i].trailer = dataTrailer.videos.results[0].key;
-						}
-						$.getJSON(url_trailer, getTrailer);
-					//Request for similar movies of each Movie
-						var url_similars = 'https://api.themoviedb.org/3/genre/'+movie.genre_ids[0]+'/movies?api_key=c0b2e256491361d28c75bbe8f9e59a85';
-						function getSimilars(dataSimilars) { 
-							data.results[i].similars_movies = dataSimilars.results;
-						}
-						$.getJSON(url_similars, getSimilars);
+		
 					//Get only the year
 						var arr = movie.release_date.split('-');
 						var date = arr[0];
 					// Variable to the DOM
 						movieHTML += '<div class="main_leftMenu_moviesList_movie">';
 						movieHTML += '<a class="btn_movie_leftMenu" data-index="'+i+'" href="javascript:void(0)">';
-						movieHTML += '<img src="http://image.tmdb.org/t/p/w500'+movie.poster_path+'">';
+						movieHTML += '<img class="img_movie_leftMenu" src="http://image.tmdb.org/t/p/w500'+movie.poster_path+'">';
 						movieHTML += '</a><h4>'+movie.title+'</h4>';
 						movieHTML += '<span>'+date+'</span>';
 						movieHTML += '</div>';
 				}); // End array cicle
 				$('#leftMenu').html(movieHTML);
 				allMovies = data.results;
+				$('.categories_favorites').remove();
 			};//End getMovies Function
 			$.getJSON(url_popular, getMovies);
+	/*UPCOMING SELECTION---------------------------------------------------------------------*/			
 		}else if(selectedOption.val() == 'upcoming') {
 			var url_upcoming = "https://api.themoviedb.org/3/movie/upcoming?api_key=c0b2e256491361d28c75bbe8f9e59a85";
 			$('#info-movie').empty();
@@ -96,33 +79,24 @@ $(function() {
 				$('.cover').css('background-size', 'cover');
 				$.each(data.results, function(i, movie){ 
 					data.results[i].favorite = false;
-					//Request for trailer of each Movie
-						var url_trailer = 'http://api.themoviedb.org/3/movie/'+movie.id+'?api_key=c0b2e256491361d28c75bbe8f9e59a85&append_to_response=videos';
-						function getTrailer(dataTrailer) {
-							data.results[i].trailer = dataTrailer.videos.results[0].key;
-						}
-						$.getJSON(url_trailer, getTrailer);
-					//Request for similar movies of each Movie
-						var url_similars = 'https://api.themoviedb.org/3/genre/'+movie.genre_ids[0]+'/movies?api_key=c0b2e256491361d28c75bbe8f9e59a85';
-						function getSimilars(dataSimilars) { 
-							data.results[i].similars_movies = dataSimilars.results;
-						}
-						$.getJSON(url_similars, getSimilars);
+
 					//Get only the year
 						var arr = movie.release_date.split('-');
 						var date = arr[0];
 					// Variable to the DOM
 						movieHTML += '<div class="main_leftMenu_moviesList_movie">';
 						movieHTML += '<a class="btn_movie_leftMenu" data-index="'+i+'" href="javascript:void(0)">';
-						movieHTML += '<img src="http://image.tmdb.org/t/p/w500'+movie.poster_path+'">';
+						movieHTML += '<img class="img_movie_leftMenu" src="http://image.tmdb.org/t/p/w500'+movie.poster_path+'">';
 						movieHTML += '</a><h4>'+movie.title+'</h4>';
 						movieHTML += '<span>'+date+'</span>';
 						movieHTML += '</div>';
 				}); // End array cicle
 				$('#leftMenu').html(movieHTML);
 				allMovies = data.results;
+				$('.categories_favorites').remove();
 			};//End getMovies Function
 			$.getJSON(url_upcoming, getMovies);
+	/*TOP RELATED SELECTION---------------------------------------------------------------------*/			
 		}else if(selectedOption.val() == 'top_rated') {
 			var url_top_rated = "https://api.themoviedb.org/3/movie/top_rated?api_key=c0b2e256491361d28c75bbe8f9e59a85";
 			$('#info-movie').empty();
@@ -134,35 +108,62 @@ $(function() {
 				$('.cover').css('background-size', 'cover');
 				$.each(data.results, function(i, movie){ 
 					data.results[i].favorite = false;
-					//Request for trailer of each Movie
-						var url_trailer = 'http://api.themoviedb.org/3/movie/'+movie.id+'?api_key=c0b2e256491361d28c75bbe8f9e59a85&append_to_response=videos';
-						function getTrailer(dataTrailer) {
-							data.results[i].trailer = dataTrailer.videos.results[0].key;
-						}
-						$.getJSON(url_trailer, getTrailer);
-					//Request for similar movies of each Movie
-						var url_similars = 'https://api.themoviedb.org/3/genre/'+movie.genre_ids[0]+'/movies?api_key=c0b2e256491361d28c75bbe8f9e59a85';
-						function getSimilars(dataSimilars) { 
-							data.results[i].similars_movies = dataSimilars.results;
-						}
-						$.getJSON(url_similars, getSimilars);
+
 					//Get only the year
 						var arr = movie.release_date.split('-');
 						var date = arr[0];
 					// Variable to the DOM
 						movieHTML += '<div class="main_leftMenu_moviesList_movie">';
 						movieHTML += '<a class="btn_movie_leftMenu" data-index="'+i+'" href="javascript:void(0)">';
-						movieHTML += '<img src="http://image.tmdb.org/t/p/w500'+movie.poster_path+'">';
+						movieHTML += '<img class="img_movie_leftMenu" src="http://image.tmdb.org/t/p/w500'+movie.poster_path+'">';
 						movieHTML += '</a><h4>'+movie.title+'</h4>';
 						movieHTML += '<span>'+date+'</span>';
 						movieHTML += '</div>';
 				}); // End array cicle
 				$('#leftMenu').html(movieHTML);
 				allMovies = data.results;
+				$('.categories_favorites').remove();
 			};//End getMovies Function
 			$.getJSON(url_top_rated, getMovies);
+	/*FAVORITES SELECTION---------------------------------------------------------------------*/		
+		}else if(selectedOption.val() == 'favorites') {
+			console.log(allMovies);
+			var movieHTML = '';
+			$.each(allMovies, function(i, movie){ 
+				if(movie.favorite) {
+					//Get only the year
+						var arr = movie.release_date.split('-');
+						var date = arr[0];
+					// Variable to the DOM
+						movieHTML += '<div class="main_leftMenu_moviesList_movie">';
+						movieHTML += '<a class="btn_movie_leftMenu" data-index="'+i+'" href="javascript:void(0)">';
+						movieHTML += '<img class="img_movie_leftMenu" src="http://image.tmdb.org/t/p/w500'+movie.poster_path+'">';
+						movieHTML += '</a><h4>'+movie.title+'</h4>';
+						movieHTML += '<span>'+date+'</span>';
+						movieHTML += '</div>';
+				}	
+			}); // End array cicle
+			$('#leftMenu').html(movieHTML);
 		}			
 	});
+
+	//When you hover a movie of the left menu get the trailer and similar movie
+	$('#leftMenu').on('mouseenter', '.btn_movie_leftMenu', function() {
+		var index = $(this).data('index');
+		//Request for trailer of each Movie
+			var url_trailer = 'http://api.themoviedb.org/3/movie/'+allMovies[index].id+'?api_key=c0b2e256491361d28c75bbe8f9e59a85&append_to_response=videos';
+			function getTrailer(dataTrailer) {
+				allMovies[index].trailer = dataTrailer.videos.results[0].key;
+			}
+			$.getJSON(url_trailer, getTrailer);
+		//Request for similar movies of each Movie
+			var url_similars = 'https://api.themoviedb.org/3/genre/'+allMovies[index].genre_ids[0]+'/movies?api_key=c0b2e256491361d28c75bbe8f9e59a85';
+			function getSimilars(dataSimilars) { 
+				allMovies[index].similars_movies = dataSimilars.results;
+			}
+			$.getJSON(url_similars, getSimilars);
+	});
+
 
 	//When you click a movie of the left menu
 	$('#leftMenu').on('click', '.btn_movie_leftMenu', function() {
@@ -198,7 +199,7 @@ $(function() {
 
 	
 			
-	//Add to favorites
+	//Add and Remove favorites
 	var index;
 	var exists = 0;
 	$('#info-movie').on('click', '.btn-favorites', function(event){
@@ -210,6 +211,22 @@ $(function() {
 			$(this).text('ADD TO FAVORITE');
 			$(this).removeClass('btn-favoritesN');
 			$(this).addClass('btn-favoritesB');
+			var movieHTML2 = '';
+			$.each(allMovies, function(i, movie){ 
+				if(movie.favorite) {
+					//Get only the year
+						var arr = movie.release_date.split('-');
+						var date = arr[0];
+					// Variable to the DOM
+						movieHTML2 += '<div class="main_leftMenu_moviesList_movie">';
+						movieHTML2 += '<a class="btn_movie_leftMenu" data-index="'+i+'" href="javascript:void(0)">';
+						movieHTML2 += '<img class="img_movie_leftMenu" src="http://image.tmdb.org/t/p/w500'+movie.poster_path+'">';
+						movieHTML2 += '</a><h4>'+movie.title+'</h4>';
+						movieHTML2 += '<span>'+date+'</span>';
+						movieHTML2 += '</div>';
+				}	
+			}); // End array cicle
+			$('#leftMenu').html(movieHTML2);
 		} else {
 			allMovies[index].favorite = true;
 			$(this).text('REMOVE FROM FAVORITES');
@@ -224,42 +241,12 @@ $(function() {
 			});
 			if(exists === 4) {
 				var selectHTML = '';
-				$('.main_leftMenu_categories').append('<option value="favorites">Favorites</option>');
+				$('.main_leftMenu_categories').append('<option class="categories_favorites" value="favorites">Favorites</option>');
 				exists = 0;
 			} else if(exists === 5) { 
-				alert('SIIIIIIII hay favoritos');
 				exists = 0;
 			}
 		}
 	}); //end favorite click function
 
-
-
-
-
-	// $('.main').on('click', function() {
-	// 	// var index = $(this).data('index');
-	// 	//Changing the background image to the cover of the first movie
-	// 	$('.main').css('background', 'url("http://image.tmdb.org/t/p/w1920'+ allMovies[0].backdrop_path +'") no-repeat');
-	// 	$('.cover').css('background-size', 'cover');
-	// 	//Changing the main info of each movie
-	// 	var mainHTML = '';
-	// 	mainHTML += '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+allMovies[0].trailer+'"></iframe>'
-	// 	mainHTML += '<div  class="main_infoMovie_description">';
-	// 	mainHTML += '<h2>'+allMovies[0].title+'</h2>';
-	// 	mainHTML += '<a href="" class="btn-favorites">ADD TO FAVORITES</a>';
-	// 	mainHTML += '<p>'+allMovies[0].overview+'</p>';
-	// 	mainHTML += '<h3>Similar Movies</h3>';
-	// 	mainHTML += '<div class="main_infomovie_similarMoviesList">';
-	// 	$.each(allMovies[0].similars_movies, function(i, data2){ 
-	// 		mainHTML += '<div class="similarMovie">';
-	// 		mainHTML += '<a href=""><img src="http://image.tmdb.org/t/p/w500'+data2.poster_path+'"></a>';
-	// 		mainHTML += '<div class="movie_info">';
-	// 		mainHTML += '<h4>'+data2.title+'</h4>';
-	// 		mainHTML += '<span>2015</span></div>';
-	// 		mainHTML += '</div>';
-	// 	});
-	// 	mainHTML += '</div></div>';
-	// 	$('#info-movie').html(mainHTML);
-	// });
 });//End Document Ready
